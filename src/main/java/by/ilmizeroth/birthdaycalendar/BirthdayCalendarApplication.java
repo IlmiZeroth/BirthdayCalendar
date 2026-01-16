13da67b77615a9bc8e8f842c371e7c9e9880e27b
GIPERFIRE <adjimetovilmi@gmail.com>
2026-01-16 10:44:50 +0300

    init commit

---
+++ src/main/java/by/ilmizeroth/birthdaycalendar/BirthdayCalendarApplication.java
@@ -0,0 +1,13 @@
+package by.ilmizeroth.birthdaycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ilmizeroth/birthdaycalendar/BirthdayCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ilmizeroth.birthdaycalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
